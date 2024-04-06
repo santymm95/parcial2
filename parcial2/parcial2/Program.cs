f8460e5c027e8d05f62fe43bd02d344bb9984f65
Santiago Montoya <santymm95@outlook.com>
2024-04-05 21:49:00 -0500

    Dependiento del numero ingresado nos va a definir la condicion que va a tomar el bucle

--- parcial2/parcial2/Program.cs
+++ parcial2/parcial2/Program.cs
@@ -8,6 +8,12 @@ class Program
 
         Console.WriteLine("¡Bienvenido al juego Adivina el número!");
         Console.Write("Ingrese el número de jugadores (entre 2 y 4): ");
+        int numJugadores;
+        while (!int.TryParse(Console.ReadLine(), out numJugadores) || numJugadores < 2 || numJugadores > 4)
+        {
+            Console.WriteLine("Por favor ingrese un número válido de jugadores (entre 2 y 4): ");
+        }
+
 
 
     }
