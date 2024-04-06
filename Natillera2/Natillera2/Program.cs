a6d2bf555b226a0590ff2d116948c8e62943ede9
Santiago Montoya <santymm95@outlook.com>
2024-04-05 22:20:37 -0500

    Ingresamo el primer mensaje y la opcion de continuar

---
+++ Natillera2/Natillera2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¡Bienvenido a la Cooperativa Navideña!");
+
+        bool continuar = true;
+
+
+
+
+
+    }
+}
