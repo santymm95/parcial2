1613789ee30ec411a0b83a98b929559f423a7ccf
Santiago Montoya <santymm95@outlook.com>
2024-04-05 21:46:44 -0500

    definimos la funcion de random y el mensaje de bienvenida

---
+++ parcial2/parcial2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        Console.WriteLine("¡Bienvenido al juego Adivina el número!");
+        Console.Write("Ingrese el número de jugadores (entre 2 y 4): ");
+
+
+    }
+}
