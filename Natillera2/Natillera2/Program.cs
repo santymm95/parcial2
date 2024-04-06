f8feeaa4105ac044e2a5e02c53297724806657d8
Santiago Montoya <santymm95@outlook.com>
2024-04-05 22:37:56 -0500

    En este for iniciamos el numero de meses, el total y el contador uno a uno, anidamos un condicional que nos acondicionara la multas

--- Natillera2/Natillera2/Program.cs
+++ Natillera2/Natillera2/Program.cs
@@ -11,8 +11,24 @@ class Program
             double ahorroTotal1 = 0, ahorroTotal2 = 0, multasTotal = 0, interesesTotal = 0;
             double prestamo1 = 0, prestamo2 = 0;
 
-             
- 
-    }
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                Console.WriteLine($"\n--- Mes {mes} ---");
+
+                double aporte1, aporte2;
+                ahorroTotal1 += RegistrarAporte(1, mes, out aporte1, out prestamo1);
+                ahorroTotal2 += RegistrarAporte(2, mes, out aporte2, out prestamo2);
+
+                if (aporte1 == 0)
+                    multasTotal += 20000;
+                if (aporte2 == 0)
+                    multasTotal += 20000;
+
+                interesesTotal += CalcularIntereses(prestamo1, mes);
+                interesesTotal += CalcularIntereses(prestamo2, mes);
+            }
+
+
+        }
      
 }
